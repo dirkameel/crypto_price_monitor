8837da0a8e470e1cba5831fd669499c634e7eaf9
anjingliaran <kidefuzambranam3115185nhgy@hotmail.com>
2024-12-15 18:57:39 +0000

    update run_monitor.sh

--- run_monitor.sh
+++ run_monitor.sh
@@ -1,9 +1,26 @@
 #!/bin/bash
-# Script to run the cryptocurrency monitor
-
-echo "Starting Cryptocurrency Price Monitor..."
-echo "Press Ctrl+C to stop the monitor"
 
 # Build and run the Go monitor
+echo "Building Go crypto monitor..."
 go build -o crypto_monitor crypto_monitor.go
-./crypto_monitor
+
+echo "Starting crypto monitor..."
+./crypto_monitor &
+MONITOR_PID=$!
+
+echo "Monitor started with PID: $MONITOR_PID"
+
+# Wait for some data to be collected
+echo "Waiting for data collection (30 seconds)..."
+sleep 30
+
+# Generate initial charts
+echo "Generating initial charts..."
+python3 chart_generator.py --summary
+
+echo "Monitor is running in background (PID: $MONITOR_PID)"
+echo "Run 'python3 chart_generator.py' to generate updated charts"
+echo "Run 'kill $MONITOR_PID' to stop the monitor"
+
+# Save PID to file for easy management
+echo $MONITOR_PID > monitor.pid
